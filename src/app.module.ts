2d62bf15f4cabb147c1606e99303d003337bbbe8
Zaid Amin <zaidaminraofin@gmail.com>
2023-03-07 20:31:56 +0600

    Remove additional files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { StudentModule } from './students/student.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { CoursesModule } from './courses/course.module';
-import { DepartmentModule } from './departments/department.module';
+import { Module } from "@nestjs/common";
+import { StudentModule } from "./students/student.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { CoursesModule } from "./courses/course.module";
+import { DepartmentModule } from "./departments/department.module";
 
 @Module({
   imports: [StudentModule, CoursesModule, DepartmentModule, TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
+    type: "postgres",
+    host: "localhost",
     port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'ums',
+    username: "postgres",
+    password: "postgres",
+    database: "aiub_portal",
     autoLoadEntities: true,
-    synchronize: true,
-  })],
-  controllers: [AppController],
-  providers: [AppService],
+    synchronize: true
+  })]
 })
 export class AppModule {}
