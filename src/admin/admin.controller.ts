5758b91a147833d5ed9dbc7a0f1bdf2ee85d91d8
Zaid Amin <zaidaminraofin@gmail.com>
2023-04-25 03:50:17 +0600

    Add admin controller

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,13 +1,116 @@
 import {
-  Body, Controller, Delete, Get, Param,
-  ParseArrayPipe, ParseEnumPipe, ParseIntPipe,
-  Post, Put, Patch, UsePipes, NotFoundException
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseArrayPipe,
+  ParseEnumPipe,
+  ParseIntPipe,
+  Post,
+  Put,
+  Patch,
+  UsePipes,
+  NotFoundException,
+  ValidationPipe,
+  Session,
+  UnauthorizedException,
+  UseInterceptors,
+  UploadedFile, ParseFilePipe, MaxFileSizeValidator, FileTypeValidator, UseGuards
 } from '@nestjs/common';
 import { AdminService } from './admin.service';
+import { CreateAdminDto } from './admin.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { SessionGuard } from '../student/session.guard';
 
-@Controller('departments')
+@Controller('admins')
 export class AdminController {
-  constructor(private readonly adminService: AdminService) {}
+  constructor(private readonly adminService: AdminService) {
+  }
 
+  @Post('register')
+  @UsePipes(new ValidationPipe())
+  register(@Body() createAdminDto: CreateAdminDto) {
+    return this.adminService.register(createAdminDto);
+  }
+
+  @Post('login')
+  @UsePipes(new ValidationPipe())
+  async login(
+    @Session() session,
+    @Body() myDto
+  ) {
+    if (await this.adminService.login(myDto)) {
+      session.email = myDto.email;
+      return { message: 'Login successful' };
+    }
+
+    return { message: 'Login failed' };
+  }
+
+  @Get('logout')
+  logout(@Session() session) {
+    if (session.destroy()) {
+      return { message: 'Logout successful' };
+    }
+
+    throw new UnauthorizedException('You are not logged in');
+  }
+
+  @Get('my-profile')
+  @UseGuards(SessionGuard)
+  profile(@Session() session) {
+      return this.adminService.myProfile(session.email);
+  }
+
+  @Get()
+  findAll() {
+    return this.adminService.findAll();
+  }
+
+  @Get(':id')
+  findById(@Param('id', ParseIntPipe) id: number) {
+    return this.adminService.findById(id);
+  }
+
+  @Delete(':id')
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.adminService.delete(id);
+  }
+
+  @Patch(':id')
+  @UsePipes(new ValidationPipe())
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateAdminDto: CreateAdminDto
+  ) {
+    return this.adminService.update(id, updateAdminDto);
+  }
+
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('myfile', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename(req, file, cb) {
+          cb(null, Date.now() + '_' + file.originalname);
+        }
+      })
+    })
+  )
+  async upload(
+    @Session() session,
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 2097152 }), // 2MB
+          new FileTypeValidator({ fileType: 'png|jpg|jpeg' })
+        ]
+      })
+    ) file: Express.Multer.File
+  ) {
+    return file;
+  }
 
 }
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,13 +1,98 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Admin } from './admin.entity';
+import { CreateAdminDto } from './admin.dto';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class AdminService {
   constructor(
     @InjectRepository(Admin)
-    private readonly adminRepo: Repository<Admin>
-  ) { }
+    private readonly adminRepo: Repository<Admin>,
+    private readonly mailerService: MailerService
+  ) {
+  }
 
+  findAll() {
+    return this.adminRepo.find();
+  }
+
+  findById(id: number) {
+    const admin = this.adminRepo.findOne({
+      where: { id }
+    });
+
+    if (!admin) {
+      throw new NotFoundException(`Admin with id: ${id} not found.`);
+    }
+
+    return admin;
+  }
+
+  async register(createAdminDto: CreateAdminDto) {
+
+    const emailData = {
+      email: createAdminDto.email,
+      subject: 'Registration Successful',
+      text: `Dear ${createAdminDto.name}, Your registration was successful.`
+    };
+
+    await this.sendEmail(emailData);
+
+    return this.adminRepo.save(createAdminDto);
+  }
+
+  async login(myDto) {
+    const admin = await this.adminRepo.findOne({
+      where: { email: myDto.email }
+    });
+
+    if (!admin) {
+      throw new NotFoundException(`Admin with email: ${myDto.email} not found.`);
+    }
+
+    if (admin.password === myDto.password) {
+      return admin;
+    }
+
+    throw new NotFoundException(`Incorrect password.`);
+  }
+
+  async sendEmail(data) {
+    await this.mailerService.sendMail({
+      to: data.email,
+      subject: data.subject,
+      text: data.text
+    });
+  }
+
+  update(id: number, updateAdminDto: CreateAdminDto) {
+    const admin = new Admin();
+    admin.name = updateAdminDto.name;
+    admin.email = updateAdminDto.email;
+    admin.password = updateAdminDto.password;
+    admin.phone = updateAdminDto.phone;
+    admin.joinDate = new Date();
+
+    return this.adminRepo.update(id, admin);
+  }
+
+  delete(id: number) {
+    const admin = this.adminRepo.findOne({
+      where: { id }
+    });
+
+    if (!admin) {
+      throw new NotFoundException(`Admin with id: ${id} not found.`);
+    }
+
+    return this.adminRepo.delete(id);
+  }
+
+  myProfile(email: string) {
+    return this.adminRepo.findOne({
+      where: { email }
+    });
+  }
 }
